2bb3f3ca7d8a8487c36d80ab51e70c12c119bf9e
traci2todd <106968550+traci2todd@users.noreply.github.com>
2022-08-02 10:21:00 -0500

    Delete mongodb_test.py

--- mongodb_test.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-url = "mongodb+srv://admin:admin@cluster0.6nvhtl1.mongodb.net/pytech"
-client = MongoClient (url)
-db = client.pytech
-print("\n -- Pytech Collection List --")
-input("\n\n End of program, press any key to exit...")
